9215a4ca605c784a8b94507b91ed676ac89961f5
George Amuzu <ga21aas@herts.ac.uk>
2022-03-13 03:10:52 +0000

    created class lesson and render to display time table

---
+++ USCBookingSystemApp/src/main/java/UI/Lesson.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gnamu
+ */
+public class Lesson {
+    private String week, name, day, period;
+    private double price;
+    private int slot;
+
+    public Lesson(String week, String name, String day, String period, double price, int slot) {
+        this.week = week;
+        this.name = name;
+        this.day = day;
+        this.period = period;
+        this.price = price;
+        this.slot = slot;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public void setSlot(int slot) {
+        this.slot = slot;
+    }
+    
+}
